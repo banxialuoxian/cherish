c35eeb498ba290581c30954a41844921e7503a3f
banxia124 <1500254340@qq.com>
2018-12-22 22:33:23 +0800

    修复无法连接数据库的bug

--- src/main/java/com/example/cherish/CherishApplication.java
+++ src/main/java/com/example/cherish/CherishApplication.java
@@ -3,11 +3,13 @@ package com.example.cherish;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
+@ServletComponentScan
 public class CherishApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/cherish/dao/StorageMapper.java
+++ src/main/java/com/example/cherish/dao/StorageMapper.java
@@ -1,12 +1,13 @@
 package com.example.cherish.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StorageMapper {
 	
-    @Select("select id from t_file_block where name = 'name'" )
-    int getStorageByName(String name);
+    @Select("select id from t_file_block where name = #{StorageName}" )
+    int getStorageByName(@Param("StorageName") String StorageName);
 
 }
